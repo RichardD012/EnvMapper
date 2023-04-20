8343f87272e116c330fef9f8207234e1a56b4330
Richard Dyer <Richard@r7development.com>
2023-04-20 13:19:58 -0600

    Adding friendly exception handling message

--- src/EnvMapper/EnvMapperException.cs
+++ src/EnvMapper/EnvMapperException.cs
@@ -11,5 +11,16 @@ namespace EnvMapper
         {
             MissingProperties = missingProperties;
         }
+
+        public string GenerateErrorString()
+        {
+            var message = Environment.NewLine;
+            message += "*************** Unable to process configuration! **************" + Environment.NewLine;
+            message += "**** The following environment variables must be defined:  ****" + Environment.NewLine;
+            message += string.Join(", ", MissingProperties);
+            message += Environment.NewLine;
+            message += "***************************************************************" + Environment.NewLine; ;
+            return message;
+        }
     }
 }
